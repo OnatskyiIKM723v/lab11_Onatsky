4a4aff6506dd91834e4f436c784bacc1eabb1c15
Администратор <@ADMIN>
2024-04-16 09:02:35 +0300

    Add project files.

---
+++ lab11_Onatsky/lab11_Onatsky.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_CTYPE, "ukr"); // дозволити використання української мови
+
+    char a, b;
+    std::cout << "введіть значення для змінної a: ";
+    std::cin >> a;
+    std::cout << "введіть значення для змінної b: ";
+    std::cin >> b;
+
+    char* ptrA = &a;
+    char* ptrB = &b;
+
+    // виведення значення a та b перед обміном
+    std::cout << "значення змінної a перед обміном: " << *ptrA << std::endl;
+    std::cout << "значення змінної b перед обміном: " << *ptrB << std::endl;
+
+    // обмін значення змінних через покажчики
+    char temp = *ptrA;
+    *ptrA = *ptrB;
+    *ptrB = temp;
+
+    // виведення значення a та b після обміну
+    std::cout << "значення змінної a після обміном: " << *ptrA << std::endl;
+    std::cout << "значення змінної b після обміном: " << *ptrB << std::endl;
+
+    return 0;
+}
